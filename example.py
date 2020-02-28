2795667534660b6182d1f7b64f4c415c73e47217
Jack O'Connor <oconnor663@gmail.com>
2020-02-28 18:14:57 -0500

    make example.py accept an argument

--- example.py
+++ example.py
@@ -13,11 +13,18 @@ subprocess.run(["cargo", "build", "--release", "--quiet"],
 # This works because ./blake3.so is a symlink to ./target/release/libblake3.so.
 import blake3  # noqa: E261
 
+# The is a file, if a command line argument is provided, or otherwise stdin.
+if len(sys.argv) > 1:
+    assert len(sys.argv) == 2
+    input_file = open(sys.argv[1], "rb")
+else:
+    input_file = sys.stdin.buffer
+
 # Hash stdin in 64 KiB chunks.
 hasher = blake3.blake3()
 buf = memoryview(bytearray(65536))
 while True:
-    n = sys.stdin.buffer.readinto(buf)
+    n = input_file.readinto(buf)
     if n == 0:
         break
     hasher.update(buf[:n])
