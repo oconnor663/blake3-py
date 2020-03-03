dc53ebd5afacfb5cf267e67bf5f7a18941385e85
Jack O'Connor <oconnor663@gmail.com>
2020-03-03 14:04:51 -0500

    explicitly delete uploaded asset after failure

--- .github/workflows/upload_github_release_asset.py
+++ .github/workflows/upload_github_release_asset.py
@@ -13,6 +13,7 @@ if tag_name.startswith(tag_prefix):
     tag_name = tag_name[len(tag_prefix):]
 assert len(sys.argv) == 2
 asset_path = sys.argv[1]
+asset_name = os.path.basename(asset_path)
 
 repo = g.get_repo("oconnor663/blake3-py")
 
@@ -27,8 +28,8 @@ else:
 try:
     print("Creating GitHub release for tag " + repr(tag_name) + "...")
     repo.create_git_release(tag_name, tag_name, tag.commit.commit.message)
-except github.GithubException as e:
-    if e.data["errors"][0]["code"] == "already_exists":
+except github.GithubException as github_error:
+    if github_error.data["errors"][0]["code"] == "already_exists":
         print("Release for tag " + repr(tag_name) + " already exists.")
     else:
         raise
@@ -43,11 +44,21 @@ else:
 print("Uploading " + repr(asset_path) + "...")
 for i in range(RETRIES):
     try:
+        print("Upload attempt #{} of {}...".format(i + 1, RETRIES))
         release.upload_asset(asset_path)
         break
-    except Exception as e:
-        print("Upload attempt #{} failed:".format(i + 1))
-        print(e)
+    except github.GithubException as github_error:
+        # Unfortunately the asset upload API is flaky. Even worse, it often
+        # partially succeeds, returning an error to the caller but leaving the
+        # release in a state where subsequent uploads of the same asset will
+        # fail with an "already_exists" error. (Though the asset is not visible
+        # on github.com, so we can't just declare victory and move on.) If we
+        # detect this case, explicitly delete the asset and continue retrying.
+        print(github_error)
+        for asset in release.get_assets():
+            if asset.name == asset_name:
+                print("Found uploaded asset after failure. Deleting...")
+                asset.delete_asset()
 else:
     raise RuntimeError("All upload attempts failed.")
 
