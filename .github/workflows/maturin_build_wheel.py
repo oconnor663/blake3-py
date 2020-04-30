cd1f63c791213449af80abb93aef765c7602688c
Jack O'Connor <oconnor663@gmail.com>
2020-04-30 14:50:05 -0400

    run Linux binary wheel builds in a manylinux Docker container
    https://github.com/oconnor663/blake3-py/issues/5
    
    I had misread the documentation at
    https://github.com/PyO3/maturin#manylinux-and-auditwheel and thought
    that Maturin would do this automatically.

--- .github/workflows/maturin_build_wheel.py
+++ .github/workflows/maturin_build_wheel.py
@@ -1,16 +1,57 @@
 #! /usr/bin/env python3
 
+import os
+import platform
 import sys
 from pathlib import Path
 import subprocess
 
 ROOT = Path(__file__).parent.parent.parent
 
-# There are generally several Python versions installed. Just package a wheel
-# for the run that this CI is explicitly testing. Also, don't build the sdist;
-# we'll do that in a separate job.
-subprocess.run(
-    ["maturin", "build", "--release", "--no-sdist", "--interpreter", sys.executable])
+# For macOS and Windows, we run Maturin against the Python interpreter that's
+# been installed and configured for this CI run, i.e. the one that's running
+# this script. (There are generally several versions installed by default, but
+# that's not guaranteed.) For Linux, in order to get "manylinux" compatibility
+# right, we need to run Maturin in a special Docker container. We hardcode
+# paths to specific interpreter versions, based on where things are installed
+# in this container. Our GitHub config has no effect on the the container, so
+# we could build all the wheels in one job, but we stick to one-wheel-per-job
+# for consistency.
+if platform.system() == "Linux":
+    version_path_components = {
+        (3, 5): "cp35-cp35m",
+        (3, 6): "cp36-cp36m",
+        (3, 7): "cp37-cp37m",
+        (3, 8): "cp38-cp38",
+        # This list needs to be kept in sync with tag.yml.
+    }
+    version_component = version_path_components[sys.version_info[:2]]
+    interpreter_path = "/opt/python/" + version_component + "/bin/python"
+    # See https://github.com/PyO3/maturin#manylinux-and-auditwheel
+    command = [
+        "docker",
+        "run",
+        "--rm",
+        "-v",
+        os.getcwd() + ":/io",
+        "konstin2/maturin:master",
+        "build",
+        "--release",
+        "--no-sdist",
+        "--interpreter",
+        interpreter_path,
+    ]
+    subprocess.run(command, check=True)
+else:
+    command = [
+        "maturin",
+        "build",
+        "--release",
+        "--no-sdist",
+        "--interpreter",
+        sys.executable,
+    ]
+    subprocess.run(command, check=True)
 
 wheels = [x for x in (ROOT / "target" / "wheels").iterdir()]
 if len(wheels) != 1:
