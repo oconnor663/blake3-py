f9c3433633d8056892f25c65c3ab67facc4af297
Jack O'Connor <oconnor663@gmail.com>
2022-10-28 00:13:14 -0700

    get rid of a Windows 3.8 hack
    It seems like this escaping is no longer needed and in fact is breaking
    the Windows 3.8 build. I'm not sure what changed, but presumably it's
    something in the latest version of setuptools.

--- c_impl/setup.py
+++ c_impl/setup.py
@@ -159,12 +159,7 @@ def prepare_extension():
         print("portable code only")
 
     def escape_preprocessor_string(s):
-        # I don't know why this Python-3.8-specific, Windows-specific
-        # workaround is needed, but it is.
-        if is_windows() and sys.version_info[:2] <= (3, 8):
-            return '\\"' + s + '\\"'
-        else:
-            return '"' + s + '"'
+        return '"' + s + '"'
 
     return setuptools.Extension(
         "blake3",
